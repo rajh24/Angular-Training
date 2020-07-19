685f18db52afa7bbc23b7bf535f79ffa5983fd0c
Raj Hawaldar <rajhawaldar007@gmail.com>
2020-07-19 22:00:00 +0530

    Initial commit with Sidebar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'dashboard', component: DashboardComponent,
+  },
+  {
+    path: 'search', component: SearchComponent
+  },
+  {
+    path: 'game', component: SearchComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
